d672eae55c538c6b64015d3b9419ea8b70cf6bbc
Jette Petzold <stu201975@mail.uni-kiel.de>
2020-09-24 12:31:16 +0200

    interactive: calculation of cons if node is moved in its own layer fixed

--- keith-interactive/src/browser/layered/relativeConstraint-utils.ts
+++ keith-interactive/src/browser/layered/relativeConstraint-utils.ts
@@ -19,31 +19,78 @@ export function setRelativeConstraint(nodes: KNode[], layers: Layer[], target: S
     const nodesOfLayer = getNodesOfLayer(layerOfTarget, nodes)
     const positionOfTarget = getPositionInLayer(nodesOfLayer, targetNode, direction)
 
-    // TODO: Randfall: Knoten war vorher schon im layer -> im bereich seiner alten position sollte kein cons gestezt werden
     const predNode = nodesOfLayer[positionOfTarget - 1]
-    const succNode = nodesOfLayer[positionOfTarget]
+    let succNode = nodesOfLayer[positionOfTarget]
+    if (succNode.id === targetNode.id && positionOfTarget !== targetNode.properties.positionId) {
+        // if targets original position is in this layer it should not be its own successor unless it is its original position
+        succNode = nodesOfLayer[positionOfTarget + 1]
+    }
 
-    let consValue: string = ""
+    let cons = determineCons(targetNode, predNode, succNode, direction)
+
+    // TODO: replace number with enum
+    switch (cons) {
+        case -1:
+            return new SetILSuccOfConstraintAction({
+                id: targetNode.id,
+                otherNode: predNode.id
+            })
+        case 1:
+            return new SetILPredOfConstraintAction({
+                id: targetNode.id,
+                otherNode: succNode.id
+            })
+        default:
+            // If the node was moved without setting a constraint - let it snap back
+            return new RefreshDiagramAction()
+    }
+
+}
+
+
+export function determineCons(target: KNode, pred: KNode, succ: KNode, direction: Direction) {
     let iLPredOf = false
     let iLSuccOf = false
-    let midY = targetNode.position.y + 0.5 * targetNode.size.height
-    let midX = targetNode.position.x + 0.5 * targetNode.size.width
+    let midY = target.position.y + 0.5 * target.size.height
+    let midX = target.position.x + 0.5 * target.size.width
+
+    let predY = 0
+    let predX = 0
+    if (!isUndefined(pred)) {
+        if (pred.id === target.id) {
+            predY = target.shadowY
+            predX = target.shadowX
+        } else {
+            predY = pred.position.y
+            predX = pred.position.x
+        }
+    }
+
+    let succY = 0
+    let succX = 0
+    if (!isUndefined(succ)) {
+        if (succ.id === target.id) {
+            succY = target.shadowY
+            succX = target.shadowX
+        } else {
+            succY = succ.position.y
+            succX = succ.position.x
+        }
+    }
 
     switch (direction) {
         case Direction.UNDEFINED:
         case Direction.LEFT:
         case Direction.RIGHT: {
-            if (isUndefined(succNode) || (!isUndefined(predNode) && midY - predNode.position.y - predNode.size.height < succNode.position.y - midY)) {
+            if (isUndefined(succ) || (!isUndefined(pred) && midY - predY - pred.size.height < succY - midY)) {
                 // distance between current node and predecessor is lower
-                if (midX < predNode.position.x + predNode.size.width && midX > predNode.position.x) {
-                    // moved node must be in certain x range
-                    consValue = predNode.id
+                if (pred.id !== target.id && midX < predX + pred.size.width && midX > predX) {
+                    // no constraint should be set if the moved node is in range of its original position & moved node must be in certain x range
                     iLSuccOf = true
                 }
-            } else {
-                if (midX < succNode.position.x + succNode.size.width && midX > succNode.position.x) {
+            } else if (!isUndefined(succ) && succ.id !== target.id) {
+                if (midX < succX + succ.size.width && midX > succX) {
                     // moved node must be in certain x range
-                    consValue = succNode.id
                     iLPredOf = true
                 }
             }
@@ -51,18 +98,15 @@ export function setRelativeConstraint(nodes: KNode[], layers: Layer[], target: S
         }
         case Direction.UP:
         case Direction.DOWN: {
-            if (isUndefined(succNode) || (!isUndefined(predNode) && midX - predNode.position.x - predNode.size.width < succNode.position.x - midX)) {
+            if (isUndefined(succ) || (!isUndefined(pred) && midX - pred.position.x - pred.size.width < succ.position.x - midX)) {
                 // distance between current node and predecessor is lower
-                if (midY < predNode.position.y + predNode.size.height && midY > predNode.position.y) {
-                    // moved node must be in certain y range
-                    consValue = predNode.id
+                if (pred.id !== target.id && midY < pred.position.y + pred.size.height && midY > pred.position.y) {
+                    // no constraint should be set if the moved node is in range of its original position & moved node must be in certain x range
                     iLSuccOf = true
                 }
-            } else {
-                // distance between current node and predecessor is lower
-                if (midY < succNode.position.y + succNode.size.height && midY > succNode.position.y) {
+            } else if (!isUndefined(succ) && succ.id !== target.id) {
+                if (midY < succ.position.y + succ.size.height && midY > succ.position.y) {
                     // moved node must be in certain y range
-                    consValue = succNode.id
                     iLPredOf = true
                 }
             }
@@ -70,18 +114,11 @@ export function setRelativeConstraint(nodes: KNode[], layers: Layer[], target: S
         }
     }
 
-    if (iLPredOf) {
-        return new SetILPredOfConstraintAction({
-            id: targetNode.id,
-            otherNode: consValue
-        })
-    } else if (iLSuccOf) {
-        return new SetILSuccOfConstraintAction({
-            id: targetNode.id,
-            otherNode: consValue
-        })
+    if (iLSuccOf) {
+        return -1
+    } else if (iLPredOf) {
+        return 1
+    } else {
+        return 0
     }
-
-    // If the node was moved without setting a constraint - let it snap back
-    return new RefreshDiagramAction()
 }
