4848c7bbc2af3c30dd5ec920e7a6f08066a22179
Jette Petzold <stu201975@mail.uni-kiel.de>
2020-09-25 13:25:16 +0200

    interactive: restructuring

--- keith-interactive/src/browser/layered/layered-relCons-view.tsx
+++ keith-interactive/src/browser/layered/layered-relCons-view.tsx
@@ -14,9 +14,9 @@ import { isUndefined } from '@theia/plugin-ext/lib/common/types';
 /** @jsx svg */
 // import { svg } from 'snabbdom-jsx';
 import { VNode } from "snabbdom/vnode";
-import { KNode } from '../constraint-classes';
+import { KNode, RelCons } from '../constraint-classes';
 import { filterKNodes, getSelectedNode } from '../helper-methods';
-import { getLayerOfNode, getLayers, getNodesOfLayer, getPositionInLayer } from './constraint-utils';
+import { getLayers } from './constraint-utils';
 import { determineCons } from './relativeConstraint-utils';
 
 export function renderRelCons(nodes: KNode[], root: KNode): VNode {
@@ -26,22 +26,15 @@ export function renderRelCons(nodes: KNode[], root: KNode): VNode {
     // let result: JSX.Element = <g></g>
 
     if (!isUndefined(selNode)) {
-        // calculate layer and position the target has in the graph at the new position
         const layers = getLayers(nodes, direction)
-        const layerOfTarget = getLayerOfNode(selNode, nodes, layers, direction)
-        const nodesOfLayer = getNodesOfLayer(layerOfTarget, nodes)
-        const positionOfTarget = getPositionInLayer(nodesOfLayer, selNode, direction)
 
-        const predNode = nodesOfLayer[positionOfTarget - 1]
-        const succNode = nodesOfLayer[positionOfTarget]
+        let cons = determineCons(nodes, layers, selNode)
 
-        let cons = determineCons(selNode, predNode, succNode, direction)
-
-        switch (cons) {
-            case -1:
+        switch (cons.relCons) {
+            case RelCons.IN_LAYER_SUCC_OF:
                 // result = <g>{result}{renderSuccOf()}</g>
                 break;
-            case 1:
+            case RelCons.IN_LAYER_PRED_OF:
                 break;
         }
     }
@@ -67,29 +60,17 @@ export function highlightNodes(root: KNode) {
     const targetNode = getSelectedNode(nodes)
 
     if (!isUndefined(targetNode)) {
-        // calculate layer and position the target has in the graph at the new position
-        const layerOfTarget = getLayerOfNode(targetNode, nodes, layers, direction)
-        const nodesOfLayer = getNodesOfLayer(layerOfTarget, nodes)
-        const positionOfTarget = getPositionInLayer(nodesOfLayer, targetNode, direction)
 
-        const predNode = nodesOfLayer[positionOfTarget - 1]
-        let succNode = nodesOfLayer[positionOfTarget]
-        if (!isUndefined(succNode) && succNode.id === targetNode.id && positionOfTarget !== targetNode.properties.positionId) {
-            // if targets original position is in this layer it should not be its own successor unless it is its original position
-            succNode = nodesOfLayer[positionOfTarget + 1]
-        }
+        let cons = determineCons(nodes, layers, targetNode)
 
-        let cons = determineCons(targetNode, predNode, succNode, direction)
-
-        // TODO: replace number with enum
-        switch (cons) {
-            case -1:
-                targetNode.highlight = true
-                predNode.highlight = true
+        switch (cons.relCons) {
+            case RelCons.IN_LAYER_SUCC_OF:
+                cons.target.highlight = true
+                cons.node.highlight = true
                 break;
-            case 1:
-                targetNode.highlight = true
-                succNode.highlight = true
+            case RelCons.IN_LAYER_PRED_OF:
+                cons.target.highlight = true
+                cons.node.highlight = true
                 break;
         }
     }
